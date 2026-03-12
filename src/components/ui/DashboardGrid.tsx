811a9f4b79f39c589091f37aa79bb2ec25d9258b
Thomas Maloney <thomasjmaloney99@gmail.com>
2026-03-11 22:42:23 -0500

    Added DashboardGrid component

---
+++ src/components/ui/DashboardGrid.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react"
+import { Grid } from "./Grid";
+
+type DashboardGridProps = {
+  children: ReactNode
+  className?: string
+}
+
+export function DashboardGrid({ children, className }: DashboardGridProps) {
+  return (
+    <Grid columns='320px 1fr' className={className}>
+      {children}
+    </Grid>
+  );
+}
