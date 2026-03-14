2ff1da013a2e79079acdbb956f21c411f9e23b20
Thomas Maloney <thomasjmaloney99@gmail.com>
2026-03-13 21:44:50 -0500

    fleshed out waypoints type a bit more

--- src/types/waypoints.ts
+++ src/types/waypoints.ts
@@ -5,10 +5,39 @@ export type WaypointTraitSymbol =
   | 'UNDER_CONSTRUCTION'
   | string;
 
+export type WaypointKind =
+  | 'PLANET'
+  | 'GAS_GIANT'
+  | 'MOON'
+  | 'ORBITAL_STATION'
+  | 'JUMP_GATE'
+  | 'ASTEROID_FIELD'
+  | 'ASTEROID'
+  | 'ENGINEERED_ASTEROID'
+  | 'ASTEROID_BASE'
+  | 'NEBULA'
+  | 'DEBRIS_FIELD'
+  | 'GRAVITY_WELL'
+  | 'ARTIFICIAL_GRAVITY_WELL'
+  | 'FUEL_STATION';
+
 export type WaypointTrait = {
   symbol: WaypointTraitSymbol;
   name: string;
   description: string;
+};
+
+export type WaypointModifierSymbol =
+  | 'STRIPPED'
+  | 'UNSTABLE'
+  | 'RADIATION_LEAK'
+  | 'CRITICAL_LIMIT'
+  | 'CIVIL_UNREST';
+
+export type WaypointModifier = {
+  name: string;
+  description: string;
+  symbol: WaypointModifierSymbol;
 };
 
 export type Waypoint = {
@@ -18,5 +47,6 @@ export type Waypoint = {
   x: number;
   y: number;
   traits: WaypointTrait[];
+  modifiers: WaypointModifier[];
   isUnderConstruction: boolean;
 };
